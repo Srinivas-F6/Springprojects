6aa642e2ab53b207acc8d866462f142279b86b21
_Srinivas_ <glsrinivas1819@gmail.com>
2026-01-24 11:25:25 +0530

    bckend projects

--- OOPs/src/employe/Tester.java
+++
@@ -1,9 +0,0 @@
-package employe;
-
-public class Tester {
-   public static void main(String[] args) {
-	   Employe e = new Employe("Srinivas",65);
-	   e.display();
-	
-   }
-}
--- security/security/src/main/java/spring/security/config/SecurityConfig.java
+++ security/security/src/main/java/spring/security/config/SecurityConfig.java
@@ -58,7 +58,6 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-	
 
 }
 
